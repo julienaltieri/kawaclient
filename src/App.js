2c8b36f655f8c91388208887e2e9ab03df1ac329
julienaltieri <julioo.altieri@gmail.com>
2023-01-08 15:32:51 -0800

    gitignore fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import BaseComponent from './components/BaseComponent'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LoginPage from './components/loginPage'
+import CategorizationRulesView from './components/CategorizationRulesView'
+import ApiCaller from './ApiCaller'
+import styled from 'styled-components'
+import UserData, { Stream } from './model'
+import Core from './core.js'
+import MasterStreamView from './components/StreamView'
+import {ModalContainer} from './ModalManager.js'
+import Navigation, {TopNavigationBar,Routes} from './components/Navigation'
+import MissionControl from './components/MissionControl'
+import SettingPage from './components/SettingPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends BaseComponent{
+  constructor(props){   
+    super(props);
+    this.state={
+      userData:Core.getUserData(),
+      loggedIn:Core.globalState.loggedIn,
+      modalController:undefined,
+      refresh: new Date()
+    }
+
+    //set callbacks for modal management
+    Core.registerApp(this)
+    Core.registerModalManagement((modalC) => this.updateState({modalController:modalC}),() => this.updateState({modalController: undefined}))
+  }
+
+
+
+  componentDidMount(){
+    //populate side navigation bar
+    Navigation.addView("Home",Routes.home);
+    Navigation.addView("Streams",Routes.streams);
+    Navigation.addView("Categorization",Routes.categorization);
+    Navigation.addView("Settings",Routes.settings);
+
+  }
+
+  render(){
+    Core.refreshTheme()
+    return (
+    <Router>
+        {!!this.state.modalController?<ModalContainer controller={this.state.modalController}/>:""}
+        <TopNavigationBar loggedIn={this.state.loggedIn}/>
+        <Switch>
+          <Route path={Routes.streams} component={MasterStreamView} exact/>
+          <Route path={Routes.categorization} component={CategorizationRulesView} exact/>
+          <Route path={Routes.home} component={MissionControl} exact/>
+          <Route path={Routes.login} component={LoginPage}/>
+          <Route path={Routes.settings} component={SettingPage}/>
+          
+        </Switch>
+    </Router>
+  )}
 }
 
-export default App;
+
+
